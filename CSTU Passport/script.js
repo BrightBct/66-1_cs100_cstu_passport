ce9c5cb007ef40c9c19c86f107f493ed9d636b38
grillhub <grill0992295002@gmail.com>
2023-10-24 06:57:42 +0700

    example 1

---
+++ CSTU Passport/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").addEventListener("click", function() {
+    window.location.href = './HTML5/index.html';
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    window.location.href = './JS/index.html';
+});
